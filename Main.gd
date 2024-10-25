ce1e60ea75807b9467e44e81446f4f2d0071f597
Alex Batlles <alexbatlles24@gmail.com>
2024-10-25 17:28:03 +0200

    Joc jugable V1

--- Main.gd
+++ Main.gd
@@ -10,7 +10,7 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	 randomize()
+	randomize()
 
 
 
@@ -22,14 +22,18 @@ func _ready():
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 func _on_ScoreTimer_timeout():
 	 score += 1
+	 $HUD.update_score(score)
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
@@ -45,6 +49,7 @@ func _on_MobTimer_timeout():
 	var direction = mob_spawn_location.rotation + PI / 2
  # Estableix la posició de la multitud a una ubicació aleatòria.
 	mob.position = mob_spawn_location.position
+	print(mob.position)
  # Afegeix una mica d'aleatorietat a la direcció.
 	direction += rand_range(-PI / 4, PI / 4)
 	mob.rotation = direction
@@ -53,3 +58,7 @@ func _on_MobTimer_timeout():
 	mob.linear_velocity = velocity.rotated(direction)
  # Va fer que la multitud s'afegís a l'escena principal.
 	add_child(mob)
+
+
+func _on_HUD_start_game():
+	pass # Replace with function body.
